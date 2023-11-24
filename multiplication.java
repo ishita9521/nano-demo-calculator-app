211179e0575de2ba1e2a5200dec1e45a11b4ea6c
ishita9521 <101060320+ishita9521@users.noreply.github.com>
2023-11-24 19:35:07 +0530

    Add multiplication file

---
+++ multiplication.java
@@ -0,0 +1,7 @@
+public class multiplication {
+    public static void main(String[] args) {
+        int a = 7, b = 6;
+        System.out.println(a * b);
+        System.out.println("Multiplication");
+    }
+}
