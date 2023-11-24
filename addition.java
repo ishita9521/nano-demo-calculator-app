ebd77e94c5ff6f71d5c1e67b9446c006a9259af2
ishita9521 <101060320+ishita9521@users.noreply.github.com>
2023-11-24 18:28:11 +0530

    Added one more file testing file

---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int add = 0;
+        add = a + b;
+        System.out.println(add);
+    }
+}
