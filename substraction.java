f003524a7709f9c0d9ee2cad83cb9268a128faba
ishita9521 <101060320+ishita9521@users.noreply.github.com>
2023-11-24 19:25:58 +0530

    Add new substraction file

---
+++ substraction.java
@@ -0,0 +1,10 @@
+public class substraction {
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+        int sub = 0;
+        sub = a - b;
+        ;
+        System.out.println(sub);
+        System.out.println("Substraction");
+    }
+}
